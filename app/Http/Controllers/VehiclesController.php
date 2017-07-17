fb15e52131b087bd9fe4ae876fb5effcdf7964d8
Sergio <esteban0513@hotmail.com>
2017-07-16 21:36:54 -0500

    Creando controlador Vehicles

---
+++ app/Http/Controllers/VehiclesController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Vehicle;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class VehiclesController extends Controller
+{
+    
+}
