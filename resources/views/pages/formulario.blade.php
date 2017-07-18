8e79f1e81b8fdea3b87fe04cfe4827680136439b
Ernesto Campos Villa <misterernest@hotmail.com>
2017-07-17 21:22:26 -0500

    Generate view form

--- resources/views/pages/formulario.blade.php
+++ resources/views/pages/formulario.blade.php
@@ -6,7 +6,46 @@
 </head>
 <body>
 	<form method="POST" action="{{url('vehicles/store')}}">
-		
+		<input type="text" id="name_user" name="name_user" placeholder="Nombre" />
+		<input type="" id="" name="email" placeholder="" />
+		<input type="" id="" name="cellphone" placeholder="" />
+		<input type="" id="" name="brand" placeholder="" />
+		<input type="" id="" name="year" placeholder="" />		
+		<input type="" id="" name="country_origin" placeholder="" />
+		<input type="" id="" name="color" placeholder="" />
+		<input type="" id="" name="placa" placeholder="" /><input type="" id="" name="ref_car" placeholder="" />
+		<input type="" id="" name="type_car" placeholder="" />
+		<input type="" id="" name="value" placeholder="" />
+		<input type="" id="" name="negociable" placeholder="" />
+		<input type="" id="" name="actual_city" placeholder="" />
+		<input type="" id="" name="actual_region" placeholder="" />
+		<input type="" id="" name="neighborhood" placeholder="" />
+		<input type="" id="" name="motor" placeholder="" />
+		<input type="" id="" name="transmision" placeholder="" />
+		<input type="" id="" name="cylinder" placeholder="" />
+		<input type="" id="" name="km" placeholder="" />
+		<input type="" id="" name="combustible" placeholder="" />
+		<input type="" id="" name="number_cylinder" placeholder="" />
+		<input type="" id="" name="tapiz" placeholder="" />
+		<input type="" id="" name="brakes" placeholder="" />
+		<input type="" id="" name="color_tapiz" placeholder="" />
+		<input type="" id="" name="cool_air" placeholder="" />
+		<input type="" id="" name="audio_systemcomputer" placeholder="" />
+		<input type="" id="" name="accesories" placeholder="" />
+		<input type="" id="" name="which_accesories" placeholder="" />
+		<input type="" id="" name="financiacion" placeholder="" />
+		<input type="" id="" name="alarm" placeholder="" />
+		<input type="" id="" name="insurance_policy" placeholder="" />
+		<input type="" id="" name="armored" placeholder="" />
+		<input type="" id="" name="satelite" placeholder="" />
+		<input type="" id="" name="photo_main" placeholder="" />
+		<input type="" id="" name="photo_2" placeholder="" />
+		<input type="" id="" name="photo_3" placeholder="" />
+		<input type="" id="" name="photo_4" placeholder="" />
+		<input type="" id="" name="photo_5" placeholder="" />
+		<input type="" id="" name="photo_6" placeholder="" />
+		<input type="" id="" name="photo_7" placeholder="" />
+		<input type="" id="" name="photo_8" placeholder="" />
 	</form>
 </body>
 </html>
