ffd4354aa4c627cf5d3fb4d19bb380ca4eb21185
Ernesto Campos Villa <misterernest@hotmail.com>
2017-07-17 20:51:00 -0500

    Generate page form

---
+++ resources/views/pages/formulario.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Autos de lujos</title>
+</head>
+<body>
+	<form method="POST" action="{{url('vehicles/store')}}">
+		
+	</form>
+</body>
+</html>
