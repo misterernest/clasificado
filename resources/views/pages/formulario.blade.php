b8f02bbd7a8103357123cabe1559c5343f7dde3f
Ernesto Campos Villa <misterernest@hotmail.com>
2017-07-20 13:27:57 -0500

    Modify view

--- resources/views/pages/formulario.blade.php
+++ resources/views/pages/formulario.blade.php
@@ -6,19 +6,27 @@
 </head>
 <body>
 	<form method="POST" action="{{url('vehicles/store')}}">
-		<input type="text" id="name_user" name="name_user" placeholder="Nombre" />
+	 	{{ csrf_field() }}
+		<input type="text" id="name_user" name="name_user" placeholder="Nombre" required="true"/>
 		<input type="" id="" name="email" placeholder="Correo electronico" />
-		<input type="" id="" name="cellphone" placeholder="Telefono celular" />
-		<input type="" id="" name="brand" placeholder="Marca" />
-		<input type="" id="" name="year" placeholder="Modelo" />		
+		<input type="" id="" name="cellphone" placeholder="Telefono celular" required="true" />
+		<input type="" id="" name="brand" placeholder="Marca" required="true"/>
+		<input type="" id="" name="year" placeholder="Modelo" required="true"/>		
 		<input type="" id="" name="country_origin" placeholder="Pais de origen" />
 		<input type="" id="" name="color" placeholder="Color" />
 		<input type="" id="" name="placa" placeholder="Placa" />
 		<input type="" id="" name="ref_car" placeholder="Serie del carro" />
-		<input type="" id="" name="type_car" placeholder="Tipo de auto" />
-		<input type="" id="" name="value" placeholder="$ Costo" />
+		<select name="">
+		   <option value=""></option> 
+		   <option value=""></option> 
+		   <option value=""></option>
+		   <option value=""></option> 
+		   <option value=""></option> 
+		   <option value=""></option> 
+		</select>
+		<input type="" id="" name="value" placeholder="$ Costo" required="true" />
 		<input type="" id="" name="negociable" placeholder="negociable" />
-		<input type="" id="" name="actual_city" placeholder="Ciudad" />
+		<input type="" id="" name="actual_city" placeholder="Ciudad" required="true"/>
 		<input type="" id="" name="actual_region" placeholder="Departamento" />
 		<input type="" id="" name="neighborhood" placeholder="Barrio" />
 		<input type="" id="" name="motor" placeholder="Motor" />
@@ -36,18 +44,19 @@
 		<input type="" id="" name="accesories" placeholder="accesorios" />
 		<input type="" id="" name="which_accesories" placeholder="Cuales" />
 		<input type="" id="" name="financiacion" placeholder="Financiamiento" />
-		<input type="" id="" name="photo_main" placeholder="" />
-		<input type="" id="" name="photo_2" placeholder="" />
-		<input type="" id="" name="photo_3" placeholder="" />
-		<input type="" id="" name="photo_4" placeholder="" />
-		<input type="" id="" name="photo_5" placeholder="" />
-		<input type="" id="" name="photo_6" placeholder="" />
-		<input type="" id="" name="photo_7" placeholder="" />
-		<input type="" id="" name="photo_8" placeholder="" />
+		<input type="" id="" name="photo_main" />
+		<input type="" id="" name="photo_2"  />
+		<input type="" id="" name="photo_3"  />
+		<input type="" id="" name="photo_4"  />
+		<input type="" id="" name="photo_5"  />
+		<input type="" id="" name="photo_6"  />
+		<input type="" id="" name="photo_7"  />
+		<input type="" id="" name="photo_8"  />
 		<input type="" id="" name="alarm" placeholder="Alarma" />
 		<input type="" id="" name="insurance_policy" placeholder="Poliza de seguro" />
 		<input type="" id="" name="armored" placeholder="Blindado" />
 		<input type="" id="" name="satelite" placeholder="Rastreo satelital" />
+		<input type="submit" value="Submit">
 	</form>
 </body>
 </html>
