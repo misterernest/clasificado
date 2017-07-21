2816561ead84ef8fb1b9010579ace50fe26ccda3
Sergio <esteban0513@hotmail.com>
2017-07-20 19:52:30 -0500

    Validaciones de campos desde el controlador VehiclesController

--- resources/views/pages/list.blade.php
+++ resources/views/pages/list.blade.php
@@ -1,7 +1,20 @@
 @forelse ($vehicles as $vehicle)
 	
-	Nombre: {!! $vehicle->name_user !!} - Email: {!! $vehicle->email !!}
-
+	Nombre: {!! $vehicle->name_user !!}
+	Email: {!! $vehicle->email !!}
+	Teléfono {!! $vehicle->cellphone !!}
+	Marca {!! $vehicle->brand !!}
+	Modelo {!! $vehicle->year !!}
+	País de origen {!! $vehicle->country_origin !!}
+	Color {!! $vehicle->color !!}
+	Serie del carro {!! $vehicle->ref_car !!}
+	Tipo de carro {!! $vehicle->type_car !!}
+	$ Costo {!! $vehicle->value !!}
+	Negociable {!! $vehicle->negociable !!}
+	Ciudad {!! $vehicle->actual_city !!}
+	Departamento {!! $vehicle->actual_region !!}
+	<br>
+	<br>
 @empty
 	
 	<h1>No hay registros</h1>
